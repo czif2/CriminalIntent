ad3ffcdc9e6d2320338a5a92e7ad1f61373ba267
8208201527 <1063003544@qq.com>
2022-11-09 08:40:26 +0800

    第十三章完成

--- app/src/main/java/com/example/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeListActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 public class CrimeListActivity extends SingleFragmentActivity{
     @Override
     protected Fragment createFragment() {
+
         return new CrimeListFragment();
     }
 }
